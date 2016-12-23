42a94d737941f3df65be79a5dded38a130f80109
Oleh Bashtovyi <bashtovyi@gmail.com>
2016-12-23 21:17:02 +0300

    added fancy+filter

---
+++ www/weinweb.pro/portfolio/filter+fancy/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+ // Скрипт  плавной прокрутки 
+	$(".scroll").on("click","a","span", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+
+	
+
+	$('#portfolio-projects').mixItUp();
+
+
+	$(".fancybox").fancybox({
+			// Default - with fix from scroll to top
+            helpers: {
+                overlay: {
+                    locked: false
+                }
+            }
+    });
+	
+
+
+});
+
+
