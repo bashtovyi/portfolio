ab18231ca2c3bc3c3dd6501bd830b1534e473164
Oleh Bashtovyi <bashtovyi@gmail.com>
2016-12-24 13:08:10 +0300

    fix contacts;

--- www/weinweb.pro/portfolio/final/js/navigation.js
+++ www/weinweb.pro/portfolio/final/js/navigation.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-  // Опеделяем переменные
   var navToggleButton = $('#navigation__button');
   var navToggleIcon = $('.navigation__toggle .fa');
   var navBlock = $('.navigation__list');
@@ -8,23 +7,23 @@ $(document).ready(function() {
   var navLink = $('.navigation__list a');
 
   // События по клику на иконку
-  navToggleButton.on('click', function(e){
+  navToggleButton.on('click', function(e) {
     e.preventDefault();
     navBlock.toggleClass(navBlockOpen);
     navButtonToggle();
   })
 
   // События по клику на ссылки
-  navLink.on('click', function(){
-    if ( navBlock.hasClass(navBlockOpen) ) {
+  navLink.on('click', function() {
+    if (navBlock.hasClass(navBlockOpen)) {
       navButtonToggle();
     }
     navBlock.removeClass(navBlockOpen);
   })
 
   // Функция для анимации иконки
-  function navButtonToggle(){
-    if ( navToggleButton.hasClass("active")) {
+  function navButtonToggle() {
+    if (navToggleButton.hasClass("active")) {
       navToggleButton.removeClass("active");
     } else {
       navToggleButton.addClass("active");
@@ -32,4 +31,4 @@ $(document).ready(function() {
   }
 
 
-}); 
+});
